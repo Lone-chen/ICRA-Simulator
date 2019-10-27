4500c569c97c1ec096eaf481df7b5532d48326d7
amber-yangcn <53104998+amber-yangcn@users.noreply.github.com>
2019-10-27 21:36:07 +0800

    Update car.py

--- car.py
+++ car.py
@@ -28,6 +28,11 @@ class car(object):
         self.hp += punishment
 
     def attacked(self,attacked,crash):
+        """
+        计算冲击扣除血量
+        ：param attacked：被击中的装甲编号 0->正面装甲 1->侧面装甲 2->背面装甲
+        : param crash: 装甲撞击到护栏的次数
+        """
         change = 0
         if attacked == 0:
             change += -20 -10*crash
@@ -40,14 +45,18 @@ class car(object):
     def change_bullet(self, change):
         self.bullet += change
 
-    def change_heat(self, v):  # 每次行动都应调用
+    def change_heat(self, v): 
+        """
+        计算枪口热量的冷却和过热惩罚
+        ：param v:子弹速度
+        """
         self.heat += v
         if self.hp >= 400:
             if (self.heat > 240) and (self.heat < 360):
                 burned = -4 * (self.heat - 240)
                 self.hp += burned
                 self.heat += self.HEAT_FREEZE * self.T
-                # 计算周期扣除血量
+                # 计算正常血量下每周期扣除血量
             elif self.heat >= 360:
                 burned = -40 * (self.heat - 360)
                 self.hp += burned
@@ -59,7 +68,7 @@ class car(object):
                 burned = -4 * (self.heat - 240)
                 self.hp += burned
                 self.heat += 2 * self.HEAT_FREEZE * self.T
-                # 计算周期扣除血量
+                # 计算低血量（hp少于400）下每周期扣除血量
             elif self.heat >= 360:
                 burned = -40 * (self.heat - 360)
                 self.hp += burned
@@ -68,18 +77,37 @@ class car(object):
                 self.heat = max(0, self.heat + 2 * self.HEAT_FREEZE * self.T)
 
     def change_local(self, change):
+        """
+        计算机器人坐标的变化
+        ：param change：x、y坐标的变化量
+        """
         self.x += change(0)
         self.y += change(1)
 
     def change_angle(self, change):
+        """
+        计算机器人底盘的变化
+        ：param change：地盘绝对角度的变化量
+        """
         self.angle += change
 
     def change_yaw(self, change):
+        """
+        计算机器人炮塔仰角的变化
+        ：param change：仰角的变化量
+        """
         self.yaw += change
 
     def change_pitch(self, change):
+         """
+        计算机器人炮塔旋转角的变化
+        ：param change：旋转角的变化量
+        """
         self.pitch += change
 
     def change_buff(self, change):
         self.buff = change
-        # 0为无buff
+        """
+        :param buff:加成效果
+        0->buff,1->回血，2->弹药补给，3->禁止移动
+        """
