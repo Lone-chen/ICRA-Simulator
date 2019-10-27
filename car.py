ab49840d2aac6bd4088a442ef97f3446f76677b3
Lone-chen <2500359212@qq.com>
2019-10-27 19:32:43 +0800

    修改了几个小车的属性，添加了一些注释示例。 修改了v_punishiment change_heat中的一些错误

--- car.py
+++ car.py
@@ -1,68 +1,71 @@
-T = 0.1 #s
-HEAT_FREEZE = -120  # 每秒
-HEAT_FREEZE_ = -240  # 血量低于400
-
 class car(object):
-    def __init__(self, bullet, angle, yaw, pitch, buff=0, hp=2000, heat=0, can_shoot=True, local=(0, 0)):
-        self.hp = hp
+    def __init__(self, bullet, angle, yaw, pitch, buff=0, hp=2000, heat=0, local=(0, 0)):
+        self.T = 0.1                # 循环周期 -> 0.1s
+        self.HEAT_FREEZE = -120     # 每秒冷却速度
+        self.hp = hp                # 小车血量
         self.bullet = bullet
         self.heat = heat
-        self.can_shoot = can_shoot
         self.x = local(0)
         self.y = local(1)
         self.angle = angle
         self.yaw = yaw
         self.pitch = pitch
         self.buff = buff
+        self.inSight = [0,0]        # 敌方车辆是否在视野内 0->不在 1->在
 
-    def v_punishment(self,v,bullet_shoot):
+    def v_punishment(self, v):
+        """
+        计算子弹发射超速惩罚
+        :param v: 子弹发射速度
+        """
         punishment = 0
         if v >= 35:
-            punishment = -2000 * bullet_shoot
+            punishment = -2000
         elif (v < 35) and (v >= 30):
-            punishment = -1000 * bullet_shoot
+            punishment = -1000
         elif (v < 30) and (v > 25):
-            punishment = -200 * bullet_shoot
-        self.hp+=punishment
+            punishment = -200
+        self.hp += punishment
 
     def attacked(self,attacked,crash):
-        change=0
+        change = 0
         if attacked == 0:
             change += -20 -10*crash
         elif attacked == 1:
             change += -40 -10*crash
         elif attacked == 2:
             change += -60 -10*crash
-        self.hp+=change
+        self.hp += change
 
     def change_bullet(self, change):
         self.bullet += change
 
-    def change_heat(self, v):  # 每次子弹发射都应调用
+    def change_heat(self, v):  # 每次行动都应调用
         self.heat += v
         if self.hp >= 400:
             if (self.heat > 240) and (self.heat < 360):
                 burned = -4 * (self.heat - 240)
                 self.hp += burned
-                self.heat += HEAT_FREEZE*T
+                self.heat += self.HEAT_FREEZE * self.T
                 # 计算周期扣除血量
-            if self.heat >= 360:
+            elif self.heat >= 360:
                 burned = -40 * (self.heat - 360)
                 self.hp += burned
                 self.heat = 360
+            else:
+                self.heat = max(0, self.heat + self.HEAT_FREEZE * self.T)
         else:
             if (self.heat > 240) and (self.heat < 360):
                 burned = -4 * (self.heat - 240)
                 self.hp += burned
-                self.heat += HEAT_FREEZE_*T
+                self.heat += 2 * self.HEAT_FREEZE * self.T
                 # 计算周期扣除血量
-            if self.heat >= 360:
+            elif self.heat >= 360:
                 burned = -40 * (self.heat - 360)
                 self.hp += burned
                 self.heat = 360
-
-    def change_canshoot(self, change):
-        self.can_shoot += change
+            else:
+                self.heat = max(0, self.heat + 2 * self.HEAT_FREEZE * self.T)
 
     def change_local(self, change):
         self.x += change(0)
