fd6c136d3ede40c850ef9e8cee5075d62a00dcd0
Lone-chen <2500359212@qq.com>
2020-01-09 19:07:59 +0800

    添加主函数

---
+++ play.py
@@ -0,0 +1,12 @@
+from main.car import CAR
+from main.input_output import observation,action
+from main.map import MAP
+import main.rewardfunction
+
+class ver_env(object):
+    def __init__(self):
+        self.build_match()
+
+
+    def build_match(self):
+        pass
