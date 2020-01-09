b99beaaa328ca528e2bf3a1e2dbd86423f83dfc8
Lone-chen <2500359212@qq.com>
2020-01-09 19:47:15 +0800

    添加主函数

--- play.py
+++ play.py
@@ -5,6 +5,10 @@ import main.rewardfunction
 
 class ver_env(object):
     def __init__(self):
+        carA = CAR(0,50,0)
+        carB = CAR()
+        map = MAP()
+
         self.build_match()
 
 
