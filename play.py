54f3d241228c56150327c3a99800c0a83c41c3e1
Lone-chen <2500359212@qq.com>
2020-01-10 14:05:57 +0800

    主函数reset()

--- play.py
+++ play.py
@@ -3,12 +3,13 @@ from main.input_output import observation,action
 from main.map import MAP
 import main.rewardfunction
 import random
+import math
 
 
 class ver_env(object):
     def __init__(self):
         self.carA = CAR(0, 50, 0, 0, local=(398, 50))
-        self.carB = CAR(1, 50, 180, 0, local=(799, 50))
+        self.carB = CAR(1, 50, math.pi/2, 0, local=(799, 50))
         self.map = MAP()
 
         self.build_match()
@@ -25,7 +26,7 @@ class ver_env(object):
                          self.carA.pitch, self.carA.debuff, self.carA.x, self.carA.y,
                          0, 0,
                          self.carB.hp, self.carB.bullet, self.carB.heat, self.carB.x, self.carB.y,
-                         self.get_isdetected(), 0, 1800)
+                         self.carB.isdetected, 0, 1800)
         sB = observation(self.carB.hp, self.carB.bullet,self.carB.heat, 0,
                          self.carB.pitch, self.carB.debuff, self.carB.x, self.carB.y,
                          0, 0,
@@ -35,8 +36,17 @@ class ver_env(object):
     def step(self):
         pass
 
-    def get_isdetected(self):
-        if random.random() >= 0.7:
-            return 1
+    def get_car_x(self, carx):
+        if carx.isdected == 1 and carx.canattack == 1:
+            return carx.x
+        elif carx.isdected == 1 and carx.canattack == 0:
+            return carx.x * random.uniform(0.97,1.03)
         else:
-            return 0
+            return -1
+    def get_car_y(self, carx):
+        if carx.isdected == 1 and carx.canattack == 1:
+            return carx.y
+        elif carx.isdected == 1 and carx.canattack == 0:
+            return carx.y * random.uniform(0.97,1.03)
+        else:
+            return -1
