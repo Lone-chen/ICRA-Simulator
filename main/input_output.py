091382a0ce28bea715a75ee2f0dd04a5ff2e4fbe
Lone-chen <2500359212@qq.com>
2020-01-10 18:58:01 +0800

    主函数step-1

--- main/input_output.py
+++ main/input_output.py
@@ -1,9 +1,9 @@
 class observation(object):
-    def __init__(self, myhp, mybullet, myheat, myfire,
-                 mypitch, mydebeff, myx, myy, mytheta,
-                 mylinearvel, myanglevel,
-                 enemyhp, enemybullet, enemyheat, enemyx, enemyy,
-                 isdetected, canattack, remaintime):
+    def __init__(self, myhp=0, mybullet=0, myheat=0, myfire=0,
+                 mypitch=0, mydebeff=0, myx=0, myy=0, mytheta=0,
+                 mylinearvel=0, myanglevel=0,
+                 enemyhp=0, enemybullet=0, enemyheat=0, enemyx=0, enemyy=0,
+                 isdetected=0, canattack=0, remaintime=0):
         self.myhp = myhp
         self.mybullet = mybullet
         self.myheat = myheat
@@ -38,4 +38,5 @@ class action(object):
     def __init__(self, actions):
         self.linear_vel = actions[0]
         self.angle_vel = actions[1]
-        self.fire = actions[2]
+        self.w = action[2]
+        self.fire = actions[3]
